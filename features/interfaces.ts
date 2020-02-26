43917d77f6f4dc65e65c3a04780baa023caedf89
Meto <licovskim@gmail.com>
2020-02-26 11:34:03 +0100

    TS Kurs

---
+++ features/interfaces.ts
@@ -0,0 +1,28 @@
+interface Reportable {
+    summary(): string /* mora da ima summary funkcija sto vrakja string*/;
+}
+
+const oldCivic = {
+    name: 'civic',
+    year: new Date(),
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`;
+    }
+};
+
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar`;
+    }
+};
+
+const printSummary = (item: Reportable): void => {
+    console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(drink);
